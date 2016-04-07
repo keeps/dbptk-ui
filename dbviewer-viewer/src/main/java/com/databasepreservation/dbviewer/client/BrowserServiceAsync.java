3dce5798ebe4b6625702d13285aff6996ba42074
Bruno Ferreira <chalkos@chalkos.net>
2016-04-07 17:23:14 +0100

    changes needed to compile using maven

---
+++ dbviewer-viewer/src/main/java/com/databasepreservation/dbviewer/client/BrowserServiceAsync.java
@@ -0,0 +1,84 @@
+package com.databasepreservation.dbviewer.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import org.roda.core.data.v2.index.IsIndexed;
+
+public interface BrowserServiceAsync {
+
+  /**
+   * GWT-RPC service asynchronous (client-side) interface
+   * 
+   * @see com.databasepreservation.dbviewer.client.BrowserService
+   */
+  void greetServer(java.lang.String name, AsyncCallback<java.lang.String> callback);
+
+  /**
+   * GWT-RPC service asynchronous (client-side) interface
+   * 
+   * @see com.databasepreservation.dbviewer.client.BrowserService
+   */
+  <T extends IsIndexed> void find(java.lang.String classNameToReturn, org.roda.core.data.adapter.filter.Filter filter,
+    org.roda.core.data.adapter.sort.Sorter sorter, org.roda.core.data.adapter.sublist.Sublist sublist,
+    org.roda.core.data.adapter.facet.Facets facets, java.lang.String localeString,
+    AsyncCallback<org.roda.core.data.v2.index.IndexResult<T>> callback);
+
+  /**
+   * GWT-RPC service asynchronous (client-side) interface
+   * 
+   * @see com.databasepreservation.dbviewer.client.BrowserService
+   */
+  void count(java.lang.String classNameToReturn, org.roda.core.data.adapter.filter.Filter filter,
+    AsyncCallback<java.lang.Long> callback);
+
+  /**
+   * GWT-RPC service asynchronous (client-side) interface
+   * 
+   * @see com.databasepreservation.dbviewer.client.BrowserService
+   */
+  <T extends IsIndexed> void retrieve(java.lang.String classNameToReturn, java.lang.String id, AsyncCallback<T> callback);
+
+  /**
+   * GWT-RPC service asynchronous (client-side) interface
+   * 
+   * @see com.databasepreservation.dbviewer.client.BrowserService
+   */
+  <T extends IsIndexed> void findRows(java.lang.String classNameToReturn, java.lang.String tableUUID,
+    org.roda.core.data.adapter.filter.Filter filter, org.roda.core.data.adapter.sort.Sorter sorter,
+    org.roda.core.data.adapter.sublist.Sublist sublist, org.roda.core.data.adapter.facet.Facets facets,
+    java.lang.String localeString, AsyncCallback<org.roda.core.data.v2.index.IndexResult<T>> callback);
+
+  /**
+   * GWT-RPC service asynchronous (client-side) interface
+   * 
+   * @see com.databasepreservation.dbviewer.client.BrowserService
+   */
+  void countRows(java.lang.String classNameToReturn, java.lang.String tableUUID,
+    org.roda.core.data.adapter.filter.Filter filter, AsyncCallback<java.lang.Long> callback);
+
+  /**
+   * GWT-RPC service asynchronous (client-side) interface
+   * 
+   * @see com.databasepreservation.dbviewer.client.BrowserService
+   */
+  <T extends IsIndexed> void retrieveRows(java.lang.String classNameToReturn, java.lang.String tableUUID, java.lang.String rowUUID,
+    AsyncCallback<T> callback);
+
+  /**
+   * Utility class to get the RPC Async interface from client-side code
+   */
+  public static final class Util {
+    private static BrowserServiceAsync instance;
+
+    public static final BrowserServiceAsync getInstance() {
+      if (instance == null) {
+        instance = (BrowserServiceAsync) GWT.create(BrowserService.class);
+      }
+      return instance;
+    }
+
+    private Util() {
+      // Utility class should not be instantiated
+    }
+  }
+}
--- dbviewer-viewer/src/main/java/com/databasepreservation/dbviewer/shared/client/ClientLoggerServiceAsync.java
+++ dbviewer-viewer/src/main/java/com/databasepreservation/dbviewer/shared/client/ClientLoggerServiceAsync.java
@@ -1,129 +1,124 @@
-/**
- * The contents of this file are based on those found at https://github.com/keeps/roda
- * and are subject to the license and copyright detailed in https://github.com/keeps/roda
- */
 package com.databasepreservation.dbviewer.shared.client;
 
-import org.roda.core.data.common.LoginException;
-import org.roda.core.data.exceptions.LoggerException;
-
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
 
-/**
- * @author Luis Faria
- *
- */
-public interface ClientLoggerServiceAsync {
+public interface ClientLoggerServiceAsync
+{
 
-  /**
-   * Log at trace level
-   *
-   * @param classname
-   * @param object
-   */
-  public void trace(String classname, String object, AsyncCallback<Void> callback);
+    /**
+     * GWT-RPC service  asynchronous (client-side) interface
+     * @see com.databasepreservation.dbviewer.shared.client.ClientLoggerService
+     */
+    void trace( java.lang.String classname, java.lang.String object, AsyncCallback<Void> callback );
 
-  /**
-   * Log at trace level
-   *
-   * @param classname
-   * @param object
-   * @param error
-   */
-  public void trace(String classname, String object, Throwable error, AsyncCallback<Void> callback);
 
-  /**
-   * Log at debug level
-   *
-   * @param classname
-   * @param object
-   */
-  public void debug(String classname, String object, AsyncCallback<Void> callback);
+    /**
+     * GWT-RPC service  asynchronous (client-side) interface
+     * @see com.databasepreservation.dbviewer.shared.client.ClientLoggerService
+     */
+    void trace( java.lang.String classname, java.lang.String object, java.lang.Throwable error, AsyncCallback<Void> callback );
 
-  /**
-   * Log at debug level
-   *
-   * @param classname
-   * @param object
-   * @param error
-   */
-  public void debug(String classname, String object, Throwable error, AsyncCallback<Void> callback);
 
-  /**
-   * Log at info level
-   *
-   * @param classname
-   * @param object
-   */
-  public void info(String classname, String object, AsyncCallback<Void> callback);
+    /**
+     * GWT-RPC service  asynchronous (client-side) interface
+     * @see com.databasepreservation.dbviewer.shared.client.ClientLoggerService
+     */
+    void debug( java.lang.String classname, java.lang.String object, AsyncCallback<Void> callback );
 
-  /**
-   * Log at info level
-   *
-   * @param classname
-   * @param object
-   * @param error
-   */
-  public void info(String classname, String object, Throwable error, AsyncCallback<Void> callback);
 
-  /**
-   * Log at warn level
-   *
-   * @param classname
-   * @param object
-   */
-  public void warn(String classname, String object, AsyncCallback<Void> callback);
+    /**
+     * GWT-RPC service  asynchronous (client-side) interface
+     * @see com.databasepreservation.dbviewer.shared.client.ClientLoggerService
+     */
+    void debug( java.lang.String classname, java.lang.String object, java.lang.Throwable error, AsyncCallback<Void> callback );
 
-  /**
-   * Log at warn level
-   *
-   * @param classname
-   * @param object
-   * @param error
-   */
-  public void warn(String classname, String object, Throwable error, AsyncCallback<Void> callback);
 
-  /**
-   * Log at error level
-   *
-   * @param classname
-   * @param object
-   */
-  public void error(String classname, String object, AsyncCallback<Void> callback);
+    /**
+     * GWT-RPC service  asynchronous (client-side) interface
+     * @see com.databasepreservation.dbviewer.shared.client.ClientLoggerService
+     */
+    void info( java.lang.String classname, java.lang.String object, AsyncCallback<Void> callback );
 
-  /**
-   * Log at error level
-   *
-   * @param classname
-   * @param object
-   * @param error
-   */
-  public void error(String classname, String object, Throwable error, AsyncCallback<Void> callback);
 
-  /**
-   * Log at fatal level
-   *
-   * @param classname
-   * @param object
-   */
-  public void fatal(String classname, String object, AsyncCallback<Void> callback);
+    /**
+     * GWT-RPC service  asynchronous (client-side) interface
+     * @see com.databasepreservation.dbviewer.shared.client.ClientLoggerService
+     */
+    void info( java.lang.String classname, java.lang.String object, java.lang.Throwable error, AsyncCallback<Void> callback );
 
-  /**
-   * Log at fatal level
-   *
-   * @param classname
-   * @param object
-   * @param error
-   */
-  public void fatal(String classname, String object, Throwable error, AsyncCallback<Void> callback);
 
-  /**
-   * Log a page hit
-   *
-   * @param pagename
-   * @throws LoginException
-   * @throws LoggerException
-   */
-  public void pagehit(String pagename, AsyncCallback<Void> callback) throws LoginException, LoggerException;
+    /**
+     * GWT-RPC service  asynchronous (client-side) interface
+     * @see com.databasepreservation.dbviewer.shared.client.ClientLoggerService
+     */
+    void warn( java.lang.String classname, java.lang.String object, AsyncCallback<Void> callback );
 
+
+    /**
+     * GWT-RPC service  asynchronous (client-side) interface
+     * @see com.databasepreservation.dbviewer.shared.client.ClientLoggerService
+     */
+    void warn( java.lang.String classname, java.lang.String object, java.lang.Throwable error, AsyncCallback<Void> callback );
+
+
+    /**
+     * GWT-RPC service  asynchronous (client-side) interface
+     * @see com.databasepreservation.dbviewer.shared.client.ClientLoggerService
+     */
+    void error( java.lang.String classname, java.lang.String object, AsyncCallback<Void> callback );
+
+
+    /**
+     * GWT-RPC service  asynchronous (client-side) interface
+     * @see com.databasepreservation.dbviewer.shared.client.ClientLoggerService
+     */
+    void error( java.lang.String classname, java.lang.String object, java.lang.Throwable error, AsyncCallback<Void> callback );
+
+
+    /**
+     * GWT-RPC service  asynchronous (client-side) interface
+     * @see com.databasepreservation.dbviewer.shared.client.ClientLoggerService
+     */
+    void fatal( java.lang.String classname, java.lang.String object, AsyncCallback<Void> callback );
+
+
+    /**
+     * GWT-RPC service  asynchronous (client-side) interface
+     * @see com.databasepreservation.dbviewer.shared.client.ClientLoggerService
+     */
+    void fatal( java.lang.String classname, java.lang.String object, java.lang.Throwable error, AsyncCallback<Void> callback );
+
+
+    /**
+     * GWT-RPC service  asynchronous (client-side) interface
+     * @see com.databasepreservation.dbviewer.shared.client.ClientLoggerService
+     */
+    void pagehit( java.lang.String pagename, AsyncCallback<Void> callback );
+
+
+    /**
+     * Utility class to get the RPC Async interface from client-side code
+     */
+    public static final class Util 
+    { 
+        private static ClientLoggerServiceAsync instance;
+
+        public static final ClientLoggerServiceAsync getInstance()
+        {
+            if ( instance == null )
+            {
+                instance = (ClientLoggerServiceAsync) GWT.create( ClientLoggerService.class );
+                ServiceDefTarget target = (ServiceDefTarget) instance;
+                target.setServiceEntryPoint( GWT.getModuleBaseURL() + "ClientLoggerService" );
+            }
+            return instance;
+        }
+
+        private Util()
+        {
+            // Utility class should not be instantiated
+        }
+    }
 }
