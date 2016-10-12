d4caef1036ab4c40d51263b2bcf11ea1d9acd020
Bruno Ferreira <chalkos@chalkos.net>
2016-10-12 16:17:23 +0100

    improved diagram

--- dbvtk-viewer/src/main/java/com/databasepreservation/visualization/client/browse/ErDiagram.java
+++ dbvtk-viewer/src/main/java/com/databasepreservation/visualization/client/browse/ErDiagram.java
@@ -9,6 +9,7 @@ import com.databasepreservation.visualization.client.ViewerStructure.ViewerDatab
 import com.databasepreservation.visualization.client.ViewerStructure.ViewerForeignKey;
 import com.databasepreservation.visualization.client.ViewerStructure.ViewerSchema;
 import com.databasepreservation.visualization.client.ViewerStructure.ViewerTable;
+import com.databasepreservation.visualization.client.common.utils.CommonClientUtils;
 import com.github.nmorel.gwtjackson.client.ObjectMapper;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.logical.shared.AttachEvent;
@@ -16,6 +17,7 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -61,19 +63,34 @@ public class ErDiagram extends Composite {
   @UiField
   FlowPanel contentItems;
 
+  final String databaseUUID;
+
   private ErDiagram(final ViewerDatabase database, final ViewerSchema schema) {
+    databaseUUID = database.getUUID();
     initWidget(uiBinder.createAndBindUi(this));
+
+    contentItems
+      .add(new HTMLPanel(
+        CommonClientUtils
+          .getFieldHTML(
+            "Using the diagram",
+            "The diagram shows the tables and relations in this schema. Each circle in the diagram represents a table, and darker circles indicate more rows and columns while their size indicates the number of relations to other tables (bigger means more relations).")));
 
     SimplePanel config = new SimplePanel();
     config.getElement().setId("erconfig");
     contentItems.add(config);
 
-    SimplePanel diagram = new SimplePanel();
+    final SimplePanel diagram = new SimplePanel();
     diagram.addStyleName("erdiagram");
     diagram.getElement().setId("erdiagram");
     diagram.addAttachHandler(new AttachEvent.Handler() {
       @Override
       public void onAttachOrDetach(AttachEvent event) {
+        // avoid setting up the diagram more than once
+        if (diagram.getStyleName().contains("initialized-erdiagram")) {
+          return;
+        }
+
         List<VisNode> visNodeList = new ArrayList<>();
         List<JsniEdge> jsniEdgeList = new ArrayList<>();
 
@@ -91,15 +108,8 @@ public class ErDiagram extends Composite {
         int minColumns = Integer.MAX_VALUE;
         int minColumnsAndRows = Integer.MAX_VALUE;
 
-        int biggestSize = 0;
         for (ViewerTable viewerTable : schema.getTables()) {
-          int size = getVisNodeSize(database, viewerTable);
-          if (biggestSize < size) {
-            biggestSize = size;
-          }
-
-          VisNode visNode = new VisNode(viewerTable.getUUID(), viewerTable.getName(), viewerTable.getDescription(),
-            size);
+          VisNode visNode = new VisNode(viewerTable.getUUID(), viewerTable.getName(), viewerTable.getDescription());
 
           visNode.numColumns = viewerTable.getColumns().size();
           visNode.numRows = new Long(viewerTable.getCountRows()).intValue();
@@ -163,28 +173,43 @@ public class ErDiagram extends Composite {
           }
         }
 
+        Double logMinColumnsAndRows = Math.log(minColumnsAndRows);
+        if (logMinColumnsAndRows.isInfinite() || logMinColumnsAndRows.isNaN()) {
+          logMinColumnsAndRows = 0.0;
+        }
+
+        Double logMaxColumnsAndRows = Math.log(maxColumnsAndRows);
+        if (logMaxColumnsAndRows.isInfinite() || logMaxColumnsAndRows.isNaN()) {
+          logMaxColumnsAndRows = 0.0;
+        }
+
         for (VisNode visNode : visNodeList) {
           visNode
             .adjustSize(getNormalizedValue(visNode.numRelationsTotal, minRelationsTotal, maxRelationsTotal, 10, 50));
 
-          visNode.adjustBackgroundColor(((double) getNormalizedValue(Math.log(visNode.numColumnsAndRows),
-            Math.log(minColumnsAndRows), Math.log(maxColumnsAndRows), 1, 30)) / 100);
+          Double logColumnsAndRows = Math.log(visNode.numColumnsAndRows);
+          if (logColumnsAndRows.isInfinite() || logColumnsAndRows.isNaN()) {
+            logColumnsAndRows = 0.0;
+          }
+
+          visNode.adjustBackgroundColor(((double) getNormalizedValue(logColumnsAndRows, logMinColumnsAndRows,
+            logMaxColumnsAndRows, 1, 70)) / 100);
         }
 
         String nodes = visNodeMapper.write(visNodeList);
         String edges = visEdgeMapper.write(jsniEdgeList);
 
-        loadDiagram(nodes, edges);
+        loadDiagram(databaseUUID, nodes, edges);
       }
     });
     contentItems.add(diagram);
   }
 
   private int getNormalizedValue(double value, double min, double max, double minNorm, double maxNorm) {
-    double range = (double) max - min;
+    double range = max - min;
     double zeroToOne = (value - min) / range;
 
-    double range2 = (double) maxNorm - minNorm;
+    double range2 = maxNorm - minNorm;
     Double result = (zeroToOne * range2) + minNorm;
     return result.intValue();
   }
@@ -227,11 +252,11 @@ public class ErDiagram extends Composite {
     int numColumns;
     int numColumnsAndRows;
 
-    public VisNode(String id, String label, String title, int size) {
+    public VisNode(String id, String label, String title) {
       this.id = id;
       this.label = label;
       this.font = new VisNodeFont(25);
-      this.size = size;
+      this.size = 20;
       this.title = title;
       this.color = new VisNodeColor();
     }
@@ -302,6 +327,7 @@ public class ErDiagram extends Composite {
       GWT.log("using " + (0.91 - value));
 
       this.color.background = "#" + hslToRgb(0.59722222222, 1.0, 0.91 - value);
+      GWT.log("------> " + this.color.background);
     }
 
     /**
@@ -322,18 +348,27 @@ public class ErDiagram extends Composite {
     public static String hslToRgb(double h, double s, double l) {
       double r, g, b;
 
-      if (s == 0f) {
+      if (s == 0.0) {
         r = g = b = l; // achromatic
       } else {
-        double q = l < 0.5f ? l * (1 + s) : l + s - l * s;
+        double q = l < 0.5 ? l * (1 + s) : l + s - l * s;
         double p = 2 * l - q;
-        r = hueToRgb(p, q, h + 1f / 3f);
+        r = hueToRgb(p, q, h + 1.0 / 3.0);
         g = hueToRgb(p, q, h);
-        b = hueToRgb(p, q, h - 1f / 3f);
+        b = hueToRgb(p, q, h - 1.0 / 3.0);
       }
       String rs = Integer.toHexString((int) (r * 255));
+      if (rs.length() == 1) {
+        rs = "0" + rs;
+      }
       String gs = Integer.toHexString((int) (g * 255));
+      if (gs.length() == 1) {
+        gs = "0" + gs;
+      }
       String bs = Integer.toHexString((int) (b * 255));
+      if (bs.length() == 1) {
+        bs = "0" + bs;
+      }
       return rs + gs + bs;
     }
 
@@ -449,20 +484,27 @@ public class ErDiagram extends Composite {
     }
   }
 
-  public static native void loadDiagram(String nodesJson, String edgesJson)
+  public static native void loadDiagram(String dbuuid, String nodesJson, String edgesJson)
   /*-{
     (function erdiagramload(){
+        // network container
+        var container = $wnd.document.getElementById('erdiagram');
+
+        // avoid setting up the diagram more than once
+        container.className += ' initialized-erdiagram';
 
         // create an array with nodes
-        var nodes = new $wnd.vis.DataSet(eval(nodesJson));
-
-        console.log(eval(nodesJson));
+        var rawNodes = eval(nodesJson);
+        var rawNodesLen = rawNodes.length;
+        for(var i=0; i<rawNodesLen; i+=1){
+            if(rawNodes[i].title == null || rawNodes[i].title.length === 0){
+                delete rawNodes[i].title;
+            }
+        }
+        var nodes = new $wnd.vis.DataSet(rawNodes);
 
         // create an array with edges
         var edges = new $wnd.vis.DataSet(eval(edgesJson));
-
-        // create a network
-        var container = $wnd.document.getElementById('erdiagram');
 
         // provide the data in the vis format
         var data = {
@@ -526,10 +568,31 @@ public class ErDiagram extends Composite {
             }//, configure: {enabled: true, showButton: true, container: $wnd.document.getElementById('erconfig')}
         };
 
-
-
         // initialize your network!
         var network = new $wnd.vis.Network(container, data, options);
+
+        network.on("selectNode", function (params) {
+            //params.event = "[original event]";
+            //console.log(params);
+
+            if(params.nodes.length == 1) {
+                //console.log("go to db" + dbuuid + " and table " + params.nodes[0]);
+                var tableuuid = params.nodes[0];
+                network.unselectAll();
+                @com.databasepreservation.visualization.shared.client.Tools.HistoryManager::gotoTable(Ljava/lang/String;Ljava/lang/String;)(dbuuid, tableuuid);
+            }
+        });
+
+        network.on("stabilized", function (params) {
+            if(params.iterations > 1){
+                options.physics.enabled = false;
+                network.setOptions(options);
+            }
+        });
+
+        network.on("dragEnd", function (params) {
+            network.unselectAll();
+        });
     })();
   }-*/;
 }
