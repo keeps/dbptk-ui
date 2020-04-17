9d1645f57e71536da6909bb068b24d8726f7ab3c
mguimaraes <mguimaraes@keep.pt>
2020-04-17 15:30:10 +0100

    Update travis trigger build script [skip ci]

--- .travis/trigger-travis-build.sh
+++ .travis/trigger-travis-build.sh
@@ -3,23 +3,40 @@
 SERVER_REPO=db-visualization-toolkit-docker
 ELECTRON_REPO=db-visualization-toolkit-electron
 
-function trigger_travis_build(){
-  REPO=$1
-  BRANCH=$2
+function build_body_request() {
+  BRANCH=$1
+  TAG=$2
 
-  echo "Triggered build for ${REPO}"
-  body="{
+  if [ -n "${TAG}" ]; then
+    CONFIG=",\"config\": {
+      \"env\": [\"TAG=$TAG\"]
+    }"
+  fi
+
+  BODY="{
    \"request\": {
     \"message\": \"This is an api request\",
     \"branch\":\"$BRANCH\"
+    $CONFIG
   }}"
+
+  echo "$BODY"
+}
+
+function trigger_travis_build(){
+  REPO=$1
+  BRANCH=$2
+  TAG=$3
+
+  echo "Triggered build for ${REPO}"
+  BODY=trigger_travis_build $2 $3
 
   curl -s -X POST \
    -H "Content-Type: application/json" \
    -H "Accept: application/json" \
    -H "Travis-API-Version: 3" \
    -H "Authorization: token $TRAVIS_TOKEN" \
-   -d "$body" \
+   -d "$BODY" \
    https://api.travis-ci.org/repo/keeps%2F$REPO/requests
 }
 
@@ -28,7 +45,7 @@ if [ "$TRAVIS_BRANCH" == "staging" ]; then
   trigger_travis_build $SERVER_REPO "staging"
 elif [ "`echo $TRAVIS_BRANCH | egrep "^v[1-9]+" | wc -l`" -eq "1" ]; then
   echo "Logic for $TRAVIS_BRANCH tag"
-  trigger_travis_build $SERVER_REPO $TRAVIS_BRANCH
+  trigger_travis_build $SERVER_REPO "master" $TRAVIS_BRANCH
 elif [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Logic for $TRAVIS_BRANCH branch"
   trigger_travis_build $SERVER_REPO $TRAVIS_BRANCH
