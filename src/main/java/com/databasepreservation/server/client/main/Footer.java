5d7540cdc995b951c54e2cfd35ced55527e5efb9
006627 <g.santos.barros@gmail.com>
2020-10-02 18:02:52 +0100

    Added footer to theme package

--- src/main/java/com/databasepreservation/server/client/main/Footer.java
+++ src/main/java/com/databasepreservation/server/client/main/Footer.java
@@ -1,10 +1,12 @@
 package com.databasepreservation.server.client.main;
 
 import com.databasepreservation.common.client.tools.HistoryManager;
+import com.databasepreservation.common.client.widgets.HTMLWidgetWrapper;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -14,20 +16,17 @@ import config.i18n.client.ClientMessages;
  * @author Bruno Ferreira <bferreira@keep.pt>
  */
 public class Footer extends Composite {
-  private static final ClientMessages messages = GWT.create(ClientMessages.class);
 
-  interface FooterUiBinder extends UiBinder<Widget, Footer> {
-  }
-
-  private static FooterUiBinder binder = GWT.create(FooterUiBinder.class);
-
-  @UiField
-  Hyperlink contributors;
+  private FlowPanel panel;
+  private HTMLWidgetWrapper layout;
 
   public Footer() {
-    initWidget(binder.createAndBindUi(this));
+    super();
 
-    contributors.setText(messages.breadcrumbTextForSponsors());
-    contributors.setTargetHistoryToken(HistoryManager.ROUTE_SPONSORS);
+    panel = new FlowPanel();
+    layout = new HTMLWidgetWrapper("Footer.html");
+
+    panel.add(layout);
+    initWidget(layout);
   }
 }
