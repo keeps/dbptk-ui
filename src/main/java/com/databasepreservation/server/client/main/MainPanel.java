dc4a1e9a67aa3c87bac98909f51d872ffbf03cda
António Lindo <alindo@keep.pt>
2025-04-23 09:41:40 +0100

    Fixed search all with / (#537)

--- src/main/java/com/databasepreservation/server/client/main/MainPanel.java
+++ src/main/java/com/databasepreservation/server/client/main/MainPanel.java
@@ -429,7 +429,7 @@ public class MainPanel extends Composite {
               && currentHistoryPath.get(2).equals(HistoryManager.ROUTE_DATABASE_SEARCH)) {
               // #database/<id>/search/<search_value>
               String databaseUUID = currentHistoryPath.get(1);
-              String searchValue = currentHistoryPath.get(3);
+              String searchValue = currentHistoryPath.get(3).replaceAll(HistoryManager.HISTORY_SEP_ESCAPE, HistoryManager.HISTORY_SEP_REGEX);
               setContent(databaseUUID, HistoryManager.ROUTE_DATABASE, currentHistoryPath.get(2),
                 new RightPanelLoader() {
                   @Override
