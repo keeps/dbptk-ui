b69b08e8ba363292b791601a3323126dd3cede5b
Alexandre Flores <alexcarvalhoflores@gmail.com>
2026-02-18 10:00:25 +0000

    Add missing ext version checks

--- src/main/java/com/databasepreservation/common/api/v1/utils/ZipOutputStreamSingleRow.java
+++ src/main/java/com/databasepreservation/common/api/v1/utils/ZipOutputStreamSingleRow.java
@@ -83,7 +83,9 @@ public class ZipOutputStreamSingleRow extends ZipOutputStream {
         nestedOriginalRowsForThisRow.put(entry.getKey(), nestedRows);
       }
       if (getDatabase().getVersion().equals(ViewerConstants.SIARD_DK_1007)
-        || getDatabase().getVersion().equals(ViewerConstants.SIARD_DK_128)) {
+        || getDatabase().getVersion().equals(ViewerConstants.SIARD_DK_128)
+        || getDatabase().getVersion().equals(ViewerConstants.SIARD_DK_1007_EXT)
+        || getDatabase().getVersion().equals(ViewerConstants.SIARD_DK_128_EXT)) {
         writeToZipFile(null, zipArchiveOutputStream, row, nestedOriginalRowsForThisRow, binaryColumns,
           nestedBinaryColumnsMap, true);
       } else {
