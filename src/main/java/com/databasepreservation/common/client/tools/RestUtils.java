45d0dcc2a6c6c6c96b5f0295afea6c2595ea5d78
Gabriel Barros <g.santos.barros@gmail.com>
2026-06-23 17:01:33 +0100

    Add report type parameter to report generation endpoints and update related components

--- src/main/java/com/databasepreservation/common/client/tools/RestUtils.java
+++ src/main/java/com/databasepreservation/common/client/tools/RestUtils.java
@@ -27,11 +27,12 @@ public class RestUtils {
     // do nothing
   }
 
-  public static SafeUri createReportResourceUri(String databaseUUID) {
+  public static SafeUri createReportResourceUri(String databaseUUID, String reportType) {
     // api/v1/database/{databaseUUID}/collection/{collectionUUID}/report
     String b = ViewerConstants.API_SERVLET + ViewerConstants.API_V1_DATABASE_RESOURCE + ViewerConstants.API_SEP
       + databaseUUID + ViewerConstants.API_SEP + ViewerConstants.API_PATH_PARAM_SOLR_COLLECTION
-      + ViewerConstants.API_SEP + databaseUUID + ViewerConstants.API_SEP + ViewerConstants.API_PATH_PARAM_REPORT;
+      + ViewerConstants.API_SEP + databaseUUID + ViewerConstants.API_SEP + ViewerConstants.API_PATH_PARAM_REPORT
+      + "?reportType=" + reportType;
     return UriUtils.fromSafeConstant(b);
   }
 
@@ -68,8 +69,6 @@ public class RestUtils {
         "presentation", ViewerConstants.PRESENTATION_EXTERNAL_SERVICE_NAME)
       + "/manifest?downloadLink=";
   }
-
-
 
   public static String createExportTableUri(String databaseUUID, String schemaName, String tableName,
     FindRequest findRequest, String zipFilename, String filename, boolean descriptions, boolean lobs,
